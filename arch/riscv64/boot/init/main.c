15390858642ed967a61430693461159b9aa53514
climatex <1714500849@qq.com>
2025-03-22 16:50:47 +0800

    .gitignore添加对tags文件的忽略 main.c删除部分无用内容

--- arch/riscv64/boot/init/main.c
+++ arch/riscv64/boot/init/main.c
@@ -62,56 +62,6 @@ int main()
 //     printf("memory management system              [OK]\n\r");
 // }
 
-// void kernel_vm_init()
-// {
-//     printf("virtual memory rage:                  %x ~ %x\n\r", 0, kernel_text_end_addr);
-//     printf("physical memory rage:                 %x ~ %x\n\r", 0, kernel_text_end_addr);
-//     extern pagetable_t kernel_pagetable;
-//     VM_MAP_INFO vm_map_info[] = {
-//         {
-//             //Map for peripherals.
-//             .virt_addr_start = 0,
-//             .phys_addr_start = 0,
-//             .size = (addr_t)kernel_start_addr,
-//             .permisson = PTE_PERMISSION_R | PTE_PERMISSION_W | PTE_PERMISSION_D | PTE_PERMISSION_A | VM_SO_STRONG_ORDER
-//         },
-//         {
-//             //Map for kernel text segment
-//             .virt_addr_start = (addr_t)kernel_start_addr,
-//             .phys_addr_start = (addr_t)kernel_start_addr,
-//             .size = (addr_t)kernel_text_end_addr - (addr_t)kernel_start_addr,
-//             .permisson = PTE_PERMISSION_R | PTE_PERMISSION_X | PTE_PERMISSION_A | VM_C_CACHEABLE | VM_B_BUFFERABLE
-//         },
-//         {
-//             //Map for the trampoline (virtual address to physical address)
-//             .virt_addr_start = (addr_t)VM_TRAMPOLINE_ADDR,
-//             .phys_addr_start = (addr_t)trampoline_start_addr,
-//             .size = PAGE_SIZE,
-//             .permisson = PTE_PERMISSION_R | PTE_PERMISSION_X | PTE_PERMISSION_A
-//         },
-//         {
-//             //Map for the trampoline (physical address to physical address)
-//             .virt_addr_start = (addr_t)trampoline_start_addr,
-//             .phys_addr_start = (addr_t)trampoline_start_addr,
-//             .size = PAGE_SIZE,
-//             .permisson = PTE_PERMISSION_R | PTE_PERMISSION_X | PTE_PERMISSION_A
-//         },
-//         {
-//             //Map for the rest of kernel and the rest of RAM
-//             .virt_addr_start = (addr_t)trampoline_end_addr,
-//             .phys_addr_start = (addr_t)trampoline_end_addr,
-//             .size = (addr_t)MEM_TOP_ADDR - (addr_t)trampoline_end_addr,
-//             .permisson = PTE_PERMISSION_R | PTE_PERMISSION_W | PTE_PERMISSION_D | PTE_PERMISSION_A
-//         }
-//     };
-//     printf("kernel pagetable create               [OK]\n\r");
-//     kernel_pagetable = pagetable_create(vm_map_info, 5);
-//     set_vm_pagetable(kernel_pagetable);
-
-//     printf("set kernel pagetable                  [OK]\n\r");
-//     printf("virtual memory system initialize      [OK]\n\r");
-// }
-
 // void kernel_interrupt_init()
 // {
 //     interrupt_init(kernel_interrupt_vector);
@@ -129,12 +79,7 @@ int main()
 
 // void kernel_init()
 // {
-//     printf("\n\r");
-//     kernel_mem_init();
-//     printf("\n\r");
-//     kernel_vm_init();
-//     printf("set kernel pagetable: %x\n\r", r_satp());
-//     printf("\n\r");
+
 //     kernel_interrupt_init();
 //     printf("\n\r");
 //     printf("kernel initialize                     [OK]\n\r");
